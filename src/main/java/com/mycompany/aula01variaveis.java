62e1a1cb2b6a322c0b5e182302c7e77b7d785bac
Gabriel_VMK <gabrielvalmorbida16@gmail.com>
2025-03-26 13:51:02 +0000

    aula 01 de variavel

---
+++ src/main/java/com/mycompany/aula01variaveis.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class aula01variaveis {
+    public static void main(String[] args) {
+        //variavel lógica
+
+        //tipo primitivo
+        boolean valorLogicoPrimitivo = true
+
+        //tipo abstrato
+        Boolean valorLogicoAbstrato = false
+    }
+}

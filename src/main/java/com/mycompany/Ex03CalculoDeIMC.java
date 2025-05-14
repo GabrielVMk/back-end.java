25d3dd472916760a0ebe8a67e014c2d4932e0040
Gabriel_VMK <gabrielvalmorbida16@gmail.com>
2025-05-14 19:41:51 +0000

    ex03

---
+++ src/main/java/com/mycompany/Ex03CalculoDeIMC.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+import java.util.Scanner;
+
+public class Ex03CalculoDeIMC {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dijite sua altura: ");
+
+        double altura1 = scan.nextInt();
+
+        System.out.println("Dijite seu peso: ");
+
+        double peso2 = scan.nextInt();
+
+
+    
+    }
+}

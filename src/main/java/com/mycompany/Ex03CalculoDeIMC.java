d7c9859240b4082773b23b39e12df641b6cf17bc
Gabriel_VMK <gabrielvalmorbida16@gmail.com>
2025-05-16 14:23:16 +0000

    Ex03-concluido

--- src/main/java/com/mycompany/Ex03CalculoDeIMC.java
+++ src/main/java/com/mycompany/Ex03CalculoDeIMC.java
@@ -4,17 +4,34 @@ import java.util.Scanner;
 
 public class Ex03CalculoDeIMC {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        System.out.println("Dijite sua altura: ");
 
-        double altura1 = scan.nextInt();
+        System.out.println("Digite sua altura: ");
+        double altura = scan.nextDouble();
 
-        System.out.println("Dijite seu peso: ");
+        System.out.println("Digite seu peso: ");
+        double peso = scan.nextDouble();
 
-        double peso2 = scan.nextInt();
+        double imc = peso / (altura * altura);
 
+        System.out.printf("Seu IMC é %.1f", imc);
 
-    
+        if (imc < 16) {
+            System.out.println("Magreza Grau III");
+        } else if (imc >= 16 && imc <= 16.9) {
+            System.out.println("Magreza Grau II");
+        } else if (imc >= 17 && imc <= 18.4){
+            System.out.println("Magreza Grau I");
+        } else if (imc >= 18.5 && imc <= 24.9){
+            System.out.println("Eutrofia");
+        } else if (imc >= 25 && imc <= 29.9){
+            System.out.println("Pré-obesidade");
+        } else if (imc >= 30 && imc <= 34.9){
+            System.out.println("Obesidade moderarda (Grau I)");
+        } else if (imc >= 35 && imc <= 39.9){
+            System.out.println("Obesidade severa (Grau II)");
+        } else {
+            System.out.println("Obesidade muito severa (Grau III)");
+        } 
     }
 }

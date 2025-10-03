1b2d684299445126140f7d701b265459215ab82e
Gabriel_VMK <gabrielvalmorbida16@gmail.com>
2025-10-03 14:36:50 +0000

    Aula 13 - Herança e Abstração inconpleto

---
+++ src/main/java/com/mycompany/poo/aula13/animais/Leao.java
@@ -0,0 +1,14 @@
+package com.mycompany.poo.aula13.animais;
+
+/**
+ * Herdar uma classe
+ * 
+ * Para herdar caracteristicas de outra classe usamos:
+ * 
+ */
+
+public class Leao extends animal {
+    public Leao(String nome, double altura, double peso) {
+        super(nome, altura, peso);
+    }
+}
